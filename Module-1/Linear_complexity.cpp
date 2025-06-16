42860c9d8293ae5ec9ff2c9ec2b1b2c576e6dfcb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-16 10:04:54 +0600

    added

--- Module-1/Linear_complexity.cpp
+++ Module-1/Linear_complexity.cpp
@@ -4,9 +4,9 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) // O(N)
     {
-        cout << i << endl;
+        cout << i << " ";
     }
 
     return 0;
