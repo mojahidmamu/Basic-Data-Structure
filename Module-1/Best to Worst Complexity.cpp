121931d3dbcc1138a4863a40ab9c7ec73e18479f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-16 12:37:45 +0600

    added

--- Module-1/Best to Worst Complexity.cpp
+++ Module-1/Best to Worst Complexity.cpp
@@ -1,10 +1,11 @@
 /*
+if: N = 1000;
 
-Constant:     O(1)
-Linear:       O(N)
-Logarithmic:  O(lonN)
-square root:  O(sqrt(N))
-quadratic:    O(N*N)
-Linearithmic: O(NlogN)
+Constant:     O(1)       => 1    *** is Rank-1
+Linear:       O(N)       => 1000
+Logarithmic:  O(lonN)    => 10
+square root:  O(sqrt(N)) => 32
+quadratic:    O(N*N)     => 1000000
+Linearithmic: O(NlogN)   => 10000
 
 */
