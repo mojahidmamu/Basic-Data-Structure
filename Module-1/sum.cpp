eceb1217cdf278bd2fd9fa955fa050c26d6f7012
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-16 09:07:55 +0600

    added

--- Module-1/sum.cpp
+++ Module-1/sum.cpp
@@ -5,10 +5,16 @@ int main()
     int n;
     cin >> n;
     int sum = 0;
-    for (int i = 1; i < n; i++)
-    {
-        sum += i;
-    }
+
+    // using Formula: 
+    sum = (n*(n-1))/2;
+
+    // using loop
+    // for (int i = 1; i < n; i++)
+    // {
+    //     sum += i;
+    // }
+
     cout << sum << endl;
     
     return 0;
