9dd71a7be1329c028ac2cf53df7b9feb75c5f7d2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-16 11:54:14 +0600

    added

--- Module-1/Quadratic complexity O(N_N).cpp
+++ Module-1/Quadratic complexity O(N_N).cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < m; j++) // O(N * M)
         {
             cout << "Mojahid" ;
         }
