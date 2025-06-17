ab969cd0ef45c6a79cd76aeeaed962421dc67d7f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 07:00:55 +0600

    added

--- Module-1/Problem_1.cpp
+++ Module-1/Problem_1.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int i = 0, sum = 0;
+    while (i < n)
+    {
+        int j = 0;
+        while (j < n)
+        {
+            sum += j;
+            j += 2;
+        }
+        i++;
+    }
+
+    cout << sum << endl;
     return 0;
 }
