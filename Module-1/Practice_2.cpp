f766c269e70dd3943f534096cda20ad88127a4e1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 07:05:50 +0600

    added

--- Module-1/Practice_2.cpp
+++ Module-1/Practice_2.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i += 10)
+    {
+        for (int j = n; j >= 0; j--)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Hi" << endl;
+    }
+
     return 0;
 }
+// O(N*N)
---
+++ Module-1/Problem_3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int j = 0;
+        while (j * j < n)
+        {
+            i + j;
+            j++;
+        }
+    }
+
+    return 0;
+}
