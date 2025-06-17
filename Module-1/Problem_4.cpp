f3ffe601f5d88cbdfebd4e4355fe322a70db3725
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 07:07:50 +0600

    added

--- Module-1/Problem_4.cpp
+++ Module-1/Problem_4.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j >= 0; j--)
+        {
+            for (int k = 1; k <= n; k++)
+            {
+                sum += i + j + k;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            sum += i + j;
+        }
+    }
+
     return 0;
 }
+// O(N*N*N)
