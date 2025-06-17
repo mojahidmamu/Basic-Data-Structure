6861fbc0914189aea1fd94d5fefedf956b0a99e3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 07:11:55 +0600

    added

--- Module-1/Problem_5.cpp
+++ Module-1/Problem_5.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i * i < n; i++)
+    {
+        sum += i;
+    }
+    int k;
+    cin >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += i;
+        i *= k;
+    }
+
     return 0;
 }
