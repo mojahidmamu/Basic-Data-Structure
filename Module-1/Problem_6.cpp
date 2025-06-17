a5122505a71068e0f361a518ddb7a0fe7b2fc021
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 07:13:23 +0600

    added

--- Module-1/Problem_6.cpp
+++ Module-1/Problem_6.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+
     return 0;
 }
+// O(N*log(N))
