054bedc32bce6183d3bc535d1c972d0fbb26884f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-17 16:56:50 +0600

    added

--- Module-1/Roborics.cpp
+++ Module-1/Roborics.cpp
@@ -2,27 +2,30 @@
 using namespace std;
 int main()
 {
-    const int mqPin   = A0;
-const int buzzer  = 7;
-const int relay   = 8;
+    const int mqPin = 'A0';
+const int buzzer = 7;
+const int relay = 8;
 int sensorValue;
 
 void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(relay, OUTPUT);
   Serial.begin(9600);
+  digitalWrite(relay, HIGH);  // প্রথমে রিলে চালু থাকবে (Ignition ON)
+  digitalWrite(buzzer, LOW);  // বুজার বন্ধ থাকবে
 }
 
 void loop() {
   sensorValue = analogRead(mqPin);
+  Serial.print("Sensor Value: ");
   Serial.println(sensorValue);
 
-  if (sensorValue > 350) {
-    digitalWrite(buzzer, HIGH);
-    digitalWrite(relay, LOW);  // ইগনিশন বন্ধ
+  if (sensorValue > 350) {        // অ্যালকোহল ডিটেকশন থ্রেশহোল্ড
+    digitalWrite(buzzer, HIGH);   // বুজার চালু
+    digitalWrite(relay, LOW);     // রিলে বন্ধ (Ignition OFF)
   } else {
-    digitalWrite(buzzer, LOW);
-    digitalWrite(relay, HIGH); // ইগনিশন চালু
+    digitalWrite(buzzer, LOW);    // বুজার বন্ধ
+    digitalWrite(relay, HIGH);    // রিলে চালু (Ignition ON)
   }
 
   delay(500);
