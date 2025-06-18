26d55c0df36bdd0a167a2941a721e89683acd67f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:20:02 +0600

    added

--- Module-2/vector_element_access.cpp
+++ Module-2/vector_element_access.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5};
-    // cout << v[i] << endl;   // access the i-th element
+    cout << v[3] << endl;   // access the i-th element
     cout << v.front() << endl; // access the first element
     cout << v.back() << endl;  // access the last element
     return 0;
