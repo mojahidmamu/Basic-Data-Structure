317c44ad7f923deac3c285e7c5cd695f08c507e7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 07:24:41 +0600

    added

--- Module-2/vector_capacity.cpp
+++ Module-2/vector_capacity.cpp
@@ -7,7 +7,9 @@ int main()
     v.push_back(10);
     cout << v.capacity() << endl; // capacity-increasing 0,1,2,4,8,,,,,,
     // v.clear(); // clear function
-    cout << v.empty() << endl;// empty function
-    cout << v.size() << endl;// size function
+    cout << v.empty() << endl; // empty function
+    cout << v.size() << endl;  // size function
+    v.resize(15, 5);           // resize function
+
     return 0;
 }
