561fe8cf3128e34f9c1b7dd4f9972ec643657e43
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:01:36 +0600

    added

--- Module-2/vector_modifier_ii.cpp
+++ Module-2/vector_modifier_ii.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
-    
+    vector<int> v = {1, 2, 3, 4, 5};
+    replace(v.begin(), v.end(), 5, 4); // replace specific element
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
     return 0;
 }
