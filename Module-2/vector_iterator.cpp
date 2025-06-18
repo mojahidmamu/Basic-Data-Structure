192b6664c97d565576d486f741c69f1b84af1ab3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:29:21 +0600

    added

--- Module-2/vector_iterator.cpp
+++ Module-2/vector_iterator.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5};
+    // v.begin(); // pointer to the first element
+    // v.end();   // pointer to the last element
+    for (auto it = v.begin(); it < v.end(); it++)
+    {
+        cout << *it << " ";
+    }
 
     return 0;
 }
