d8b80889250fefa9aea3c581aca8a208b9318446
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:55:16 +0600

    added

--- Module-2/vector_of_string.cpp
+++ Module-2/vector_of_string.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i]<< " ";
+    }
+
     return 0;
 }
