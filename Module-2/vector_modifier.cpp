82ce08f2a6976c0e6f3e1b54ec416a2e901db261
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 07:36:47 +0600

    added

--- Module-2/vector_modifier.cpp
+++ Module-2/vector_modifier.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    vector<int> v = {1, 2, 3, 4, 5};
+    vector<int> v2;
+    v2 = v; // value assign
+    // v2.pop_back(); // remove last element
+    v2.insert(v2.begin() + 2, 10);      // add element in specific position
+    v2.erase(v2.begin() + 1, v2.end()); // delete from specific position
+    for (int i = 0; i < v2.size(); i++)
+    {
+        cout << v2[i] << " ";
+    }
+
     return 0;
 }
