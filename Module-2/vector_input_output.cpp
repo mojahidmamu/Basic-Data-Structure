1395d6709f227602f7d806a980ce6cee31718511
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:36:07 +0600

    added

--- Module-2/vector_input_output.cpp
+++ Module-2/vector_input_output.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
+    // int arr[n];
+    vector<int> v(n); // vector 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    
     
     return 0;
 }
