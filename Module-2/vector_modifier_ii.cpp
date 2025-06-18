2331c2226cf19bd7cca46b3894b77b18aba83b11
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-18 08:08:54 +0600

    added

--- Module-2/vector_modifier_ii.cpp
+++ Module-2/vector_modifier_ii.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5};
-    replace(v.begin(), v.end(), 5, 4); // replace specific element
+    // replace(v.begin(), v.end(), 5, 4); // replace specific element
+    auto it = find(v.begin(), v.end(), 100);// find specific element in whole vector: 
+    
+
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
