9602aaf7ef460930a727987e29c46f0567fc1224
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-20 06:47:10 +0600

    added

--- Module-2/D_Counting_Elements.cpp
+++ Module-2/D_Counting_Elements.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i + 1] == v[i])
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+
     return 0;
 }
