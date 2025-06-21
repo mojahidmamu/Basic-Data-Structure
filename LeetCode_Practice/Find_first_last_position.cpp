a5b88579609ed021185224687ad78da038496672
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-21 08:57:28 +0600

    added

---
+++ LeetCode_Practice/Find_first_last_position.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            cout << i << " ";
+        }
+        
+    }
+    
+    
+    return 0;
+}
