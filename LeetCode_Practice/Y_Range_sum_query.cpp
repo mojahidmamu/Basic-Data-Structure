14c145d7683b7537e5897fc7dedaf7b2e3281c87
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-21 10:32:07 +0600

    added

--- LeetCode_Practice/Y_Range_sum_query.cpp
+++ LeetCode_Practice/Y_Range_sum_query.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
+    int N, Q;
+    cin >> N, Q;
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    for (int i = 0; i < Q; i++)
+    {
+        int L, R;
+        cin >> L, R;
+    }
+    
+    
     
     return 0;
 }
