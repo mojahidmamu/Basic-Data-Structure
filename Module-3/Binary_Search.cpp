744415bc091aecef1adaa853e5d53fa3e246dd95
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-22 08:39:35 +0600

    added

--- Module-3/Binary_Search.cpp
+++ Module-3/Binary_Search.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
+    int N, Q;
+    cin >> N >> Q;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < Q; i++)
+    {
+        int X;
+        cin >> X;
+    }
+    
     
     return 0;
 }
