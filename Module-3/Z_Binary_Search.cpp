55842ff2cdab6450e5b06baf91a9ec3e0a8e08e0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-22 10:00:23 +0600

    added

---
+++ Module-3/Z_Binary_Search.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, Q;
+    cin >> N >> Q;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < Q; i++)
+    {
+        int X;
+        cin >> X;
+        int flag = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == X)
+            {
+                flag = 1;
+            }
+        }
+        if (flag == 1)
+        {
+            cout << "found" << endl;
+        }
+        else
+        {
+            cout << "not found" << endl;
+        }
+    }
+
+    return 0;
+}
