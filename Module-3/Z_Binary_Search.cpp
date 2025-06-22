270b69613abae1557234a806ef6ac95cb42e1765
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-22 10:30:49 +0600

    added

--- Module-3/Z_Binary_Search.cpp
+++ Module-3/Z_Binary_Search.cpp
@@ -9,23 +9,40 @@ int main()
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < Q; i++)
+    sort(arr, arr + N); // O(logN)
+
+    for (int i = 0; i < Q; i++) // O(Q)
     {
         int X;
         cin >> X;
         int flag = 0;
-        for (int i = 0; i < N; i++)
+
+        // Binary Search: O(logN)
+        int low = 0, high = N - 1;
+        while (low <= high) // this is Condition
         {
-            if (arr[i] == X)
+            int mid_index = (low + high) / 2; // mid-index
+
+            if (arr[mid_index] == X)
             {
-                flag = 1;
+                flag = 1; // if value is founded,
+                break;    // then loop is break;
+            }
+            else if (arr[mid_index] > X)
+            {
+                high = mid_index - 1;
+            }
+            else if (arr[mid_index] < X)
+            {
+                low = mid_index + 1;
             }
         }
-        if (flag == 1)
+
+        if (flag == 1) // if flag=1,
         {
             cout << "found" << endl;
         }
-        else
+        else           // if flag is not found
         {
             cout << "not found" << endl;
         }
