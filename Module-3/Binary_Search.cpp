2ab81f717b8c877b49a16ee25b0a1cba8f59afab
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-22 08:42:58 +0600

    added

--- Module-3/Binary_Search.cpp
+++ Module-3/Binary_Search.cpp
@@ -13,8 +13,23 @@ int main()
     {
         int X;
         cin >> X;
+        int flag = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == X)
+            {
+                flag = 1;
+            }
+        }
+        if (flag == 1)
+        {
+            cout << "found" << endl;
+        }
+        else
+        {
+            cout << "not found" << endl;
+        }
     }
-    
-    
+
     return 0;
 }
