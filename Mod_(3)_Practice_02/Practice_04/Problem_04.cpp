7c2f70b3869bcd10a70044ee855ac9329096d8b2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-24 11:06:35 +0600

    added

--- Mod_(3)_Practice_02/Practice_04/Problem_04.cpp
+++ Mod_(3)_Practice_02/Practice_04/Problem_04.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    for (int i = n / 2; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j = j * 2)
+        {
+            cout << i << j << endl;
+        }
+    }
+
     return 0;
 }
+// O(NlogN)
--- Mod_(3)_Practice_02/Practice_04/Problem_05.cpp
+++ Mod_(3)_Practice_02/Practice_04/Problem_05.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    for (int i = n / 2; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j = j++)
+        {
+            cout << i << j << endl;
+        }
+    }
+
     return 0;
 }
+// O(N*N)
