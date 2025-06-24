20903af58df14ef24199d617d292309643fc188e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-24 10:37:37 +0600

    added

--- Mod_(3)_Practice_02/Practice_05.cpp
+++ Mod_(3)_Practice_02/Practice_05.cpp
@@ -1,16 +1,30 @@
 // Sum of 1 from N;
+
+// With loop -> not allowed in time complexity
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int N;
+//     cin >> N;
+//     int sum = 0;
+//     for (int i = 1; i <= N; i++)
+//     {
+//         sum += i;
+//     }
+//     cout << sum << endl;
+
+//     return 0;
+// }
+
+// Without loop -> fact (Time Complexity):
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int N;
     cin >> N;
-    int sum = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        sum += i;
-    }
+    int sum = N * (N + 1) / 2;
     cout << sum << endl;
-
     return 0;
 }
