3346b662de9e63826dbba8391a299df14cb6aaa2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-24 10:49:43 +0600

    added

--- Mod_(3)_Practice_02/Practice_03.cpp
+++ Mod_(3)_Practice_02/Practice_03.cpp
@@ -1,8 +1,22 @@
-// Search Query: 
+// Search Query:
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+        int X;
+        cin >> X;
+    }
+
     return 0;
 }
