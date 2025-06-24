1446db63dcb7faf63060e44620081523fc944713
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-24 11:03:30 +0600

    added

--- Mod_(3)_Practice_02/Practice_04/Problem_03.cpp
+++ Mod_(3)_Practice_02/Practice_04/Problem_03.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    ;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            for (int k = j; k > 0; k--)
+            {
+                cout << i << j << k;
+            }
+        }
+    }
+
     return 0;
 }
