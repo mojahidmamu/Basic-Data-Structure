8ee02fe580ba89627d1124237afbc73e2ed45399
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-25 09:43:11 +0600

    added

--- Mod_(3)_Practice_02/Frequency Counter.cpp
+++ Mod_(3)_Practice_02/Frequency Counter.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    map<int, int> freq;
+
+    for (int i = 0; i < n; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    for (auto it : freq)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
+
     return 0;
 }
