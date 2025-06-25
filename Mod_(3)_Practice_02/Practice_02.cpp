4a034d1fd6980550824b6f0784cd897ee4aa95fe
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-25 09:00:04 +0600

    added

--- Mod_(3)_Practice_02/Practice_02.cpp
+++ Mod_(3)_Practice_02/Practice_02.cpp
@@ -1,8 +1,20 @@
-// Equilibrium Index 
+// Equilibrium Index
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
     
     return 0;
 }
