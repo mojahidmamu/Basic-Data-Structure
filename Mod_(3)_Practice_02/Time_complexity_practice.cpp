6270c4bb104c5a05f9d3e46b2e4c843da49ab532
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-25 11:03:00 +0600

    added

--- Mod_(3)_Practice_02/Time_complexity_practice.cpp
+++ Mod_(3)_Practice_02/Time_complexity_practice.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
+    /*
     int k = 1;
     while (k <= n)
     {
         cout << k << endl;
         k = k * 2;
     }
+        O(lonN)
+    */
 
     return 0;
 }
