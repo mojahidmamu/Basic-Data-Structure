dcd051440f7787416efb2920d474ca229e44faf8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-25 09:17:32 +0600

    added

--- Mod_(3)_Practice_02/L_New_Array.cpp
+++ Mod_(3)_Practice_02/L_New_Array.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 int main()
 {
+    int N;
+    cin >> N;
+    int Arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> Arr[i];
+    }
+    int Brr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> Brr[i];
+    }
+
+    //  Concatanation: 
     
+
+    int Crr[2 * N];
+    for (int i = 0; i < N * 2; i++)
+    {
+        cout << Crr[i] << " ";
+    }
+
     return 0;
 }
