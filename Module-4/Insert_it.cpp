630f71cef2643906a883c3b67b1240e4cdea1cc0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-26 09:31:15 +0600

    added

--- Module-4/Insert_it.cpp
+++ Module-4/Insert_it.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
-    
+    // input first array:
+    vector<int> N;
+    cin >> N; // input->N
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    // input second array:
+    vector<int> M;
+    cin >> M; // input->M
+    int B[M];
+    for (int i = 0; i < M; i++)
+    {
+        cin >> B[i];
+    }
+    //
+    vector<int> index;
+    cin >> index; // input index:
+
+    A.insert(A.begin() + index, B);
+
+    for (int i = 0; i < N + M; i++)
+    {
+        cout << A[i] << " ";
+    }
+
     return 0;
 }
