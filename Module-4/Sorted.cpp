96a94134b94ed9f4185d29c6a58973e79f022def
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-26 09:13:41 +0600

    added

--- Module-4/Sorted.cpp
+++ Module-4/Sorted.cpp
@@ -2,6 +2,37 @@
 using namespace std;
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int flag = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < arr[i-1])
+            {
+                 flag = 0;
+            }
+            
+        }
+
+        if (flag == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
