31eb8b5060ccf7884409e8c9f9fe55f74ab3d53d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-26 08:03:12 +0600

    added

--- Module-4/Duplicate.cpp
+++ Module-4/Duplicate.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-    
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int flag = 0;
+    for (int i = 0; i < N; i++)
+    {
+        /* code */
+    }
+
+    if (flag == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
