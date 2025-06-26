16f1ad498ea741d3285723438fe2a924d56f3818
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-26 10:36:32 +0600

    added

--- Module-4/Printing_X.cpp
+++ Module-4/Printing_X.cpp
@@ -4,6 +4,30 @@ int main()
 {
     int N;
     cin >> N;
-    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i == j && i + j == N - 1)
+            {
+                cout << "X";
+            }
+            else if (i == j)
+            {
+                cout << "\\";
+            }
+            else if (i + j == N - 1)
+            {
+                cout << "/";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
