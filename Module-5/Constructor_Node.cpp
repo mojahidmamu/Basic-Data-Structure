c6073a06f9b90ec8151c2ccc20fb82eaa88a4ea9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-30 12:10:24 +0600

    added

---
+++ Module-5/Constructor_Node.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val) // constructor এ value টা নিচ্ছি।
+    {
+
+        this->val = val;   // value টা class variable এ assign করছি।
+        this->next = NULL; // যেহেতু প্রতিটি nodeই আলাদা তাই by default zero/NULL set করে দিচ্ছি।
+    }
+};
+
+int main()
+{
+    Node a(10); // এখানে a object এ 10 value এর একটা node create করছি।
+    Node b(20); // এখানে b object এ 20 value এর একটা node create করছি।
+
+    a.next = &b; // এখানে b node এর address a এর next/pointer এ রাখা হচ্ছে। যাতে a দিয়ে b কে access করা যায়।
+
+    cout << a.val << endl; // এখানে a এর value print করে দেখলাম।
+    cout << b.val << endl; // এখানে b এর value print করে দেখলাম।
+    cout << (*a.next).val << endl; // এখানে b এর value a কে dereference করে print করে দেখলাম।
+    cout << a.next->val << endl; // এখানে b এর value a কে অন্যভাবে dereference করে print করে দেখলাম।
+
+    return 0;
+}
--- Module-5/Dynamic_Node.cpp
+++ Module-5/Dynamic_Node.cpp
@@ -11,19 +11,18 @@ public:
         this->next = NULL;
     }
 };
+
 int main()
 {
-    Node a(10), b(20), c(30);
-    // a.val = 10;
-    // b.val = 20;
-    // c.val = 30;
+    Node* head = new Node(10);
+     
 
-    a.next = &b;
-    b.next = &c;
+    // a.next = &b;
+    // b.next = &c;
     // c.next = NULL;
 
-    cout << a.val << endl;
-    cout << a.next->val << endl;
-    cout << a.next->next->val << endl;
+    // cout << a.val << endl;
+    // cout << a.next->val << endl;
+    // cout << a.next->next->val << endl;
     return 0;
 }
