ae8ad5229a3f5c8be1194b4b697eab974484a1a6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-01 12:31:23 +0600

    added

--- Module-6/Node_Insert.cpp
+++ Module-6/Node_Insert.cpp
@@ -1,7 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void intsert_at_tail(Node *head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
 int main()
 {
-    
+    Node *head = NULL;
+
+    intsert_at_tail(head, 1);
+    intsert_at_tail(head, 3);
+
+    print(head);
     return 0;
 }
