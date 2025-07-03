0fed17b6452dc0c30a06db6ed92cb02eefb1ab0b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-03 17:50:24 +0600

    added

--- Module-7/Practice_Day_02/Problem_4.cpp
+++ Module-7/Practice_Day_02/Problem_4.cpp
@@ -11,8 +11,68 @@ public:
         this->next = NULL;
     }
 };
+
+// insert at any position:
+void insert_at_any_pos(Node *&head, int idx, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 0; i < idx - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+
+// insert at head:
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+// insert at tail:
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail =  newNode;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+// print fun:
+void print_liked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+    }
+    tmp = tmp->next;
+}
+
 int main()
 {
-    
+    // input from user:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    // print function:
+    print_liked_list(head);
     return 0;
 }
