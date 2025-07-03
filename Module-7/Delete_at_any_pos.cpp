5c65ad7f6a2a3f7eda0a69bc606c0417c13f38a0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-03 23:48:15 +0600

    added

---
+++ Module-7/Delete_at_any_pos.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// insert at tail:
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+
+// This is a function, which delete head node: (first node at Linked_List):-
+void delete_at_head(Node *&head)
+{
+    Node *delete_node = head;
+    head = head->next;
+    delete delete_node;
+}
+
+// print fun:
+void print_liked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, val);
+        }
+    };
+    // call this function:
+    delete_at_head(head);
+    print_liked_list(head);
+
+    return 0;
+}
