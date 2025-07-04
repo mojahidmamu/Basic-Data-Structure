23f39834013704384ee53dd4fd8c085791860f05
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-04 11:35:32 +0600

    added

--- Module-7/Practice_Day_02/Problem_5.cpp
+++ Module-7/Practice_Day_02/Problem_5.cpp
@@ -1,7 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// insert at tail:
+int sz = 0;
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+        sz++;
+    }
+};
+
+// sort_function:
+void sort_dec(Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val < j->val)
+            {
+                int tmp = i->val;
+                i->val = j->val;
+                j->val = tmp;
+            }
+        }
+    }
+};
+
+// print fun:
+void print_liked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
 int main()
 {
-    
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (cin >> val && val != -1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    sort_dec(head);
+    print_liked_list(head);
+
     return 0;
 }
