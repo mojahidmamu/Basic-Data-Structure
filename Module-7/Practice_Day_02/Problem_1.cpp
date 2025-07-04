615eb85ed837a535e47c1e3d088ad6c658a89167
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-04 07:29:51 +0600

    added

--- Module-7/Practice_Day_02/Problem_1.cpp
+++ Module-7/Practice_Day_02/Problem_1.cpp
@@ -12,8 +12,62 @@ public:
     }
 };
 
+// insert at tail:
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+
+
+
+// print fun:
+void print_liked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
 int main()
 {
+    Node *head = NULL;
+    Node *tail = NULL;
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
     
+    int val;
+    while (cin >> val && val != -1)
+    {
+        // if (val == -1)
+        // {
+        //     break;
+        // }
+        insert_at_tail(head, tail, val);
+        
+    }
+    //////
+    int val2;
+    while (cin >> val2 && val2 != -1)
+    {
+        insert_at_tail(head2, tail2, val2);
+        
+    }
+    print_liked_list(head); 
+    print_liked_list(head2); 
+
     return 0;
 }
