33d535a90cb0f8f94948df8d0376f33455f6830e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-06 10:26:56 +0600

    added

--- Module-8/Same_to_Same.cpp
+++ Module-8/Same_to_Same.cpp
@@ -12,8 +12,84 @@ public:
     }
 };
 
+// insert at tail:
+int sz = 0;
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+        sz++;
+    }
+};
+
+// print fun:
+void print_liked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
+// get length:
+int get_length(Node *head)
+{
+    int count = 1;
+    while (head != NULL)
+    {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
 int main()
 {
-    
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+
+    int val;
+    while (cin >> val && val != -1)
+    {
+        // if (val == -1)
+        // {
+        //     break;
+        // }
+        insert_at_tail(head1, tail1, val);
+    }
+    //////
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+
+    int val2;
+    while (cin >> val2 && val2 != -1)
+    {
+        insert_at_tail(head2, tail2, val2);
+    }
+    // print_liked_list(head);
+    // print_liked_list(head2);
+
+    int len1 = get_length(head1);
+    int len2 = get_length(head2);
+    if (len1 == len2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
