a92145df7a4b25b01ff06ff4fed839db15b7240f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-06 09:47:00 +0600

    added

--- Module-8/Get Difference.cpp
+++ Module-8/Get Difference.cpp
@@ -12,8 +12,83 @@ public:
     }
 };
 
+// insert at tail:
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+
+// Find the Maximum value:
+int max_value(Node *head)
+{
+    int maxVal = head->val;
+    Node *tmp = head->next;
+    while (tmp != NULL)
+    {
+        if (tmp->val > maxVal)
+        {
+            maxVal = tmp->val;
+        }
+        else
+        {
+            tmp = tmp->next;
+        }
+    }
+    return maxVal;
+};
+
+// Find the Minimum value:
+int min_value(Node *head)
+{
+    int minVal = head->val;
+    Node *tmp = head->next;
+    while (tmp != NULL)
+    {
+        if (tmp->val < minVal)
+        {
+            minVal = tmp->val;
+        }
+        else
+        {
+            tmp = tmp->next;
+        }
+    }
+    return minVal;
+}
+
 int main()
 {
 
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, val);
+        }
+    }
+
+    int max = max_value(head);
+    int min = min_value(head);
+    int result = max - min;
+    cout << result << endl;
     return 0;
 }
