a0dd4f555ada54fd59715f3315cc41e7977e65a3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-07 07:28:19 +0600

    added

--- Module-9/Doubly_Linked_List.cpp
+++ Module-9/Doubly_Linked_List.cpp
@@ -5,14 +5,28 @@ class Node
 public:
     int val;
     Node *next;
+    Node *prev;
     Node(int val)
     {
         this->val = val;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
+
 int main()
 {
-    
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+    tail->next = NULL;
+
+    head->prev = NULL;
+    a->prev = head;
+    tail->prev = a;
+
     return 0;
 }
