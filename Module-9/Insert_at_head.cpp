89cfabc7a7cdfbbc1507ba9c9f2bb7db4b01c607
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-07 14:56:25 +0600

    added

---
+++ Module-9/Insert_at_head.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// print-forward:
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
+// print-backward:
+void print_backward(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    a->prev = head;
+    tail->prev = a;
+
+    cout << "Forward Print: ";
+    print_forward(head);
+    cout << "Backward Print: ";
+    print_backward(tail);
+
+    return 0;
+}
