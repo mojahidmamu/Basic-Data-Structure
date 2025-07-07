86a120b0ce113758769ca57ce061698ef6c1e703
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-07 15:13:36 +0600

    added

---
+++ Module-9/Insert_at_any_pos.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// insert at tail; 
+void insert_at_tail(Node *&tail, int val)
+{
+    Node * newNode = new Node(val);
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+// print-forward:
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    b->next = tail;
+
+    a->prev = head;
+    b->prev = a;
+    tail->prev = b;
+
+    insert_at_tail(tail, 100);
+
+    cout << "Forward Print: ";
+    print_forward(head);
+    
+
+    return 0;
+}
