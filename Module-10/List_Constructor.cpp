5e672614daf89931be0b3a90260e0ccc2aff11b8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 19:38:32 +0600

    added

--- Module-10/List_Constructor.cpp
+++ Module-10/List_Constructor.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 int main()
 {
-    //Type-1: List Constructor: 
+    // Type-1: List Constructor:
     list<int> l;
     list<char> l;
     list<float> l;
     list<long long int> l;
+
+    // Type-2: List with value:
+    list<int> l(100);
+    list<char> l(50);
+    list<float> l(30);
+    list<long long int> l(40);
     return 0;
 }
