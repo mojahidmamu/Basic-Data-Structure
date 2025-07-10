b2dd226226b718b2d1d345d37770f59fe21b19f2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 11:15:22 +0600

    added

---
+++ Module-10/STL.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l(100);
+    list<int> v(100, 10);
+    list<char> v(50, 'a');
+    list<float> v(30, 2.5);
+    list<long long> v(40, 100);
+    cout << l.size() << endl;
+    return 0;
+}
