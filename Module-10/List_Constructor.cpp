666624a7623019a70840ca095bacef0ca33cf463
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 19:39:27 +0600

    added

--- Module-10/List_Constructor.cpp
+++ Module-10/List_Constructor.cpp
@@ -8,7 +8,14 @@ int main()
     list<float> l;
     list<long long int> l;
 
-    // Type-2: List with value:
+    // Type-2: List with size:
+    list<int> l(100);
+    list<char> l(50);
+    list<float> l(30);
+    list<long long int> l(40);
+
+
+    // Type-3: List with size & value: 
     list<int> l(100);
     list<char> l(50);
     list<float> l(30);
