0930e299063fdabdced85fb533f61a9f1ded7fd6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 19:36:44 +0600

    added

--- Module-10/List_Constructor.cpp
+++ Module-10/List_Constructor.cpp
@@ -1,8 +1,8 @@
-// //  Type 1 :
-// //   list<data_type> list_name;
-
-// //   Code : 
-// list<int> my_list;           // এর মাধ্যমে ইন্টিজার ডাটা টাইপের একটি list তৈরি হবে
-//     list<char> my_list;      // এর মাধ্যমে ক্যারেক্টার ডাটা টাইপের একটি list তৈরি হবে
-//     list<float> my_list;     // এর মাধ্যমে ফ্লোট ডাটা টাইপের একটি list তৈরি হবে
-//     list<long long> my_list; // এর মাধ্যমে লং লং ইন্টিজার ডাটা টাইপের একটি list তৈরি হবে     
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //Type-1: List Constructor: 
+    list<int> l;
+    return 0;
+}
