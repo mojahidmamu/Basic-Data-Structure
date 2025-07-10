7a34d9fb1c982a3e5b1766eba63752eeeec6af26
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 19:57:18 +0600

    added

--- Module-10/List_Modifier.cpp
+++ Module-10/List_Modifier.cpp
@@ -10,10 +10,13 @@ int main()
     }
 
     // myList= or myList.assign(list2.begin(),list2.end())
-    
+
     // l.push_back();  // add in tail
     // l.push_front(); // add in head
     // l.pop_front();  // delete in head
     // l.pop_back();  // delete in tail
+    // l.insert() // insert element in specific position: 
+    // l.erase(); // delete element from ith to specific position: 
+    
     return 0;
 }
