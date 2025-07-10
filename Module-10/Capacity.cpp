0120185d3cda07e40849ca262edb664f8fff3204
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 11:33:57 +0600

    added

--- Module-10/Capacity.cpp
+++ Module-10/Capacity.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 int main()
 {
-    
+    /**
+     * list< int > my_list(10) ;
+        cout << my_list.size() << endl ; // returns 10 .
+     */
+
+
+     
+    /*
+    list< int > my_list ;
+    cout << my_list.max_size() << endl ; // এটি একটি ১০^৯ এর কাছাকাছি একটি ভ্যালু রিটার্ন করে .
+    */
+
+
+
+    /***
+     * list < int > my_list = {1,2,3,4} ; // একটি List ডিক্লেয়ার করে সেখানে এই ভ্যালু গুলা দিয়ে ইনিশিয়ালাইজ করলাম
+cout << my_list.size() << endl ; // যেহেতু এখানে ৪ টি ভ্যালু আছে তাই এই list সাইজ ৪ রিটার্ন করবে
+my_list.clear() ; // List ক্লিয়ার করা হলো
+cout << my_list.size() << endl ; // list টি ক্লিয়ার করার কারণে তার সাইজ ০ প্রিন্ট হবে
+     */
+
+
+
+     /*
+     list < int > my_list = {1,2,3,4} ; // একটি list ডিক্লেয়ার করে সেখানে এই ভ্যালু গুলা দিয়ে ইনিশিয়ালাইজ করলাম
+cout << my_list.empty() << endl ; // ০ প্রিন্ট হবে কারণ my_list খালি নয়
+my_list.clear() ; // List টি খালি করা হলো
+cout << my_list.empty() << endl ; // ১ প্রিন্ট হবে কারণ my_list খালি।
+
+     */
     return 0;
 }
