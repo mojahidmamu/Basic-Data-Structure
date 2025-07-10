aa3c84954e4509adbed81af1bc2ee7a7d498583b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 20:08:10 +0600

    added

--- Module-10/List_Element_Access.cpp
+++ Module-10/List_Element_Access.cpp
@@ -5,8 +5,9 @@ int main()
     list<int> l(10, 5);
 
     // l.back() //Access the tail element.
-    l.front() // Access the head element.
-   
+    // l.front() // Access the head element.
+    // *next(l.begin(), 2) // Access the ith element
+
     for (int element : l)
     {
         cout << element << " ";
