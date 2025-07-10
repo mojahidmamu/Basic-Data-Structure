fb22f17dc27ecb2ce1dd27cfb9bc0b7729983fad
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 20:06:32 +0600

    added

--- Module-10/List_Element_Access.cpp
+++ Module-10/List_Element_Access.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main()
 {
     list<int> l(10, 5);
+
+    // l.back() //Access the tail element.
+    l.front() // Access the head element.
    
     for (int element : l)
     {
