206331648de0f38500a8cab4e8c2e6d058aa36f9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-10 19:48:22 +0600

    added

--- Module-10/Capacity.cpp
+++ Module-10/Capacity.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main()
 {
      list<int> l(100);
-     cout << l.size() << endl;
-    return 0;
+     // cout << l.size() << endl;
+     // cout << l.max_size() << endl;
+     // l.clear();
+     // cout << l.size() << endl;
+     // cout << l.empty() << endl;
+     return 0;
 }
