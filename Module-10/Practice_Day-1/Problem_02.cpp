7558de04abaadcb2a552953bc67944ac8f5bf596
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-11 16:24:18 +0600

    added

--- Module-10/Practice_Day-1/Problem_02.cpp
+++ Module-10/Practice_Day-1/Problem_02.cpp
@@ -1,4 +1,4 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
@@ -30,55 +30,31 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
         newNode->prev = tail;
         tail = newNode;
     }
-}
+};
 
-// Compare two doubly linked lists node-by-node
-bool are_equal(Node *head1, Node *head2)
+void print_linked_list(Node *head)
 {
-    while (head1 != NULL && head2 != NULL)
+    Node *tmp = head;
+    while (tmp != NULL)
     {
-        if (head1->val != head2->val)
-        {
-            return false;
-        }
-        head1 = head1->next;
-        head2 = head2->next;
+        cout << tmp->val << " ";
+        tmp = tmp->next;
     }
-    // If one of them still has elements, they are not equal
-    return (head1 == NULL && head2 == NULL);
-}
+    cout << endl;
+};
 
 int main()
 {
-    Node *head1 = NULL;
-    Node *tail1 = NULL;
+    Node *head = NULL;
+    Node *tail = NULL;
     int x;
 
     // Input for first list
     while (cin >> x && x != -1)
     {
-        insert_at_tail(head1, tail1, x);
+        insert_at_tail(head, tail, x);
     }
 
-    Node *head2 = NULL;
-    Node *tail2 = NULL;
-    int y;
-
-    // Input for second list
-    while (cin >> y && y != -1)
-    {
-        insert_at_tail(head2, tail2, y);
-    }
-
-    // Compare both lists
-    if (are_equal(head1, head2))
-    {
-        cout << "YES" << endl;
-    }
-    else
-    {
-        cout << "NO" << endl;
-    }
-
+    print_linked_list(head);
     return 0;
 }
