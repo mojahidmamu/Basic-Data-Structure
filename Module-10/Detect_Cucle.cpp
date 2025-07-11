4348c5af185e1d00b49386db4577a5223813140a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-11 11:59:50 +0600

    added

--- Module-10/Detect_Cucle.cpp
+++ Module-10/Detect_Cucle.cpp
@@ -14,6 +14,30 @@ public:
 
 int main()
 {
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    // Connections: 
+    head->next = a;
+    a->next = b;
+
+    // take two pointer: 
+    Node *slow = head;
+    Node *fast = head;
+    bool flag = false; // take a flag variable:
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+        {
+            flag = 
+        }
+        
+    }
     
+
     return 0;
 }
