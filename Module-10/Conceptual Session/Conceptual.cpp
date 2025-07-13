98a5adcce687b4f5b79c36ce357fd05896c0cc36
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-13 18:21:22 +0600

    added

--- Module-10/Conceptual Session/Conceptual.cpp
+++ Module-10/Conceptual Session/Conceptual.cpp
@@ -5,15 +5,48 @@ class Node
 public:
     int val;
     Node *next;
+    Node *prev;
     Node(int val)
     {
         this->val = val;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
-int main()
+void insert_at_tail(Node *head, Node *tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void print_doubly(Node *head)
 {
     
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (cin >> val)
+    {
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    print_doubly(head);
     return 0;
 }
