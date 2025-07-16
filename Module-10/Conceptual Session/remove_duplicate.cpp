75fa8eff2862238c801e8fb9fca906abfceda6a5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-16 09:40:28 +0600

    added

---
+++ Module-10/Conceptual Session/remove_duplicate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l;
+    int n;
+    cin >> n;
+    while (n == -1)
+    {
+        l.push_back(n);
+    }
+    l.sort();
+    l.unique();
+    for (auto val : l)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
