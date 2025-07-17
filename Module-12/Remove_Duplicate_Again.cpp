635b73f5043d8f53f58d6b99bd00aa90e00293c6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-17 18:10:07 +0600

    added

--- Module-12/Remove_Duplicate_Again.cpp
+++ Module-12/Remove_Duplicate_Again.cpp
@@ -12,8 +12,45 @@ public:
     }
 };
 
+// insert at tail;
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+// Print:
+void print_fun(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
 int main()
 {
-    
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (cin >> val && val != -1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    print_fun(head);
+
     return 0;
 }
