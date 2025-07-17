487ec214b994d78d09d4bbca7561ff599c93226c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-17 16:44:44 +0600

    added

--- Module-11/Delete_a_Node_from_Linked_List.cpp
+++ Module-11/Delete_a_Node_from_Linked_List.cpp
@@ -14,13 +14,13 @@ public:
 
 int main()
 {
-    
+
     return 0;
 }
 /**
- * Condition:Check for duplicate >>>>  
+ * Condition:Check for duplicate >>>>
  * 1. head == NULL; => return head...
- * 2. tmp.next == NULL; => 
+ * 2. tmp.next == NULL; =>
  * 3. if > tmp.val == tmp.next.val > {
  *     *deleteNode == tmp.next
  *      tmp.next == tmp.next.next;
@@ -28,4 +28,21 @@ int main()
  * }
  * 4. else if >  tmp.val != tmp.next.val > tmp = tmp.next
  * 5. else if > tmp.next == NULL > break;
+ */
+
+
+
+ 
+/**
+ * Condition: Check for Delete a node in a specific position:
+ *
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;   // নোডের মধ্যে তার নেক্সট নোডের ভেলু রেখে দিতে পারি।
+        ListNode *deleteNode = node->next;  // নেক্সট নোডকে একটি ডিলিট নোড পয়েন্টারে রেখে দিতে পারি, যেহেতু এটি আমরা ডিলিট করব।
+        node->next = node->next->next;   // নোডের নেক্সটে তার নেক্সটের নেক্সট নোডকে কানেক্ট করে দিতে পারি।
+        delete deleteNode;   // এরপর ডিলিটনোডকে ডিলিট করে দিলেই হয়ে যাবে।
+    }
+};
+
  */
