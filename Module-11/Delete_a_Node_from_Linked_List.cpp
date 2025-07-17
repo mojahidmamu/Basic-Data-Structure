783216424a3f47f8dcfb1b913cdc0d76a089931f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-17 16:25:41 +0600

    added

--- Module-11/Delete_a_Node_from_Linked_List.cpp
+++ Module-11/Delete_a_Node_from_Linked_List.cpp
@@ -17,3 +17,15 @@ int main()
     
     return 0;
 }
+/**
+ * Condition: 
+ * 1. head == NULL; => return head...
+ * 2. tmp.next == NULL; => 
+ * 3. if > tmp.val == tmp.next.val > {
+ *     *deleteNode == tmp.next
+ *      tmp.next == tmp.next.next;
+ *      delete deleteNode;
+ * }
+ * 4. else if >  tmp.val != tmp.next.val > tmp = tmp.next
+ * 5. else if > tmp.next == NULL > break;
+ */
