ea448b69ce8095d448fde59c4f83be2a0b8386af
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-18 18:34:29 +0600

    added

--- Module-12/Remember_Previous_Queries.cpp
+++ Module-12/Remember_Previous_Queries.cpp
@@ -1,52 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
-{
-public:
-    int val;
-    Node *next;
-    Node *prev;
-    Node(int val)
-    {
-        this->val = val;
-        this->next = NULL;
-        this->prev = NULL;
-    }
-};
-
-// insert at head;
-void insert_at_head(Node *&head, Node *&tail, int val)
-{
-    Node *newNode = new Node(val);
-    Node *tmp = head;
-    if (tmp == NULL)
-    {
-        head = newNode;
-        tail = newNode;
-    }
-    else
-    {
-        newNode->next = head;
-        head->prev = newNode;
-        head = newNode;
-    }
-}
-
-// insert at tail:
-void insert_at_tail(Node *&tail, int val)
-{
-    Node *newNode = new Node(val);
-    Node *tmp = tail;
-    if (tmp->next == NULL)
-    {
-        tmp->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
-    }
-    tmp = tmp->next;
-}
-
 // Print forward:
 void print_forward(Node *head)
 {
@@ -75,23 +29,25 @@ void print_backward(Node *tail)
 
 int main()
 {
-    Node *head = NULL;
-    Node *tail = NULL;
-
-    int query;
-    cin >> query;
-    while (query--)
+    list<int> myList; 
+    int Q;
+    cin >> Q;
+    while (Q--)
     {
         int X, V;
         cin >> X >> V;
+
         if (X == 0)
         {
-            insert_at_head(head, tail, V);
+
         }
         else if (X == 1)
         {
-            insert_at_tail(tail, V);
         }
+        else if (X == 2)
+        {
+        }
+
         print_forward(head);
         print_backward(tail);
     }
