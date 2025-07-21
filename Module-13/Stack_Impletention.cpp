36b72adcd3ed8f0787859b6767dded3b33d42b52
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-21 15:02:18 +0600

    added

--- Module-13/Stack_Impletention.cpp
+++ Module-13/Stack_Impletention.cpp
@@ -5,10 +5,12 @@ class Node
 public:
     int val;
     Node *next;
+    Node *prev;
     Node(int val)
     {
         this->val = val;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
