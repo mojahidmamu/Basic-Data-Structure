597c2a5ec8bb2be434d688c4b590f94f8f299168
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-22 09:58:57 +0600

    added

--- Module-13/Stack_Impletention.cpp
+++ Module-13/Stack_Impletention.cpp
@@ -4,7 +4,18 @@ class myStack
 {
 public:
     vector<int> v;
-    
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        v.back(); // access the last element
+    }
 };
 
 int main()
