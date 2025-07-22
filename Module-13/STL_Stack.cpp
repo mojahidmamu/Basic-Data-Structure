272193114e0ff1e20eb48671147555e625d6f350
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-22 11:26:33 +0600

    added

--- Module-13/STL_Stack.cpp
+++ Module-13/STL_Stack.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
+    // vector<int> v;
+    // list<int> l;
+    stack<int> st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+    // print : 
+    while (st.empty() == false)
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    
     
     return 0;
 }
