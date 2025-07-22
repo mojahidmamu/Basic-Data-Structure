f40a05cafdb6be2ffed9c54097940a918d225091
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-22 12:01:32 +0600

    added

--- Module-13/Stack_implemetation_with_Doubly_Linked_List.cpp
+++ Module-13/Stack_implemetation_with_Doubly_Linked_List.cpp
@@ -14,9 +14,38 @@ public:
     }
 };
 
+class myStack
+{
+public:
+    list<int> l;
+
+    // There are only 5 operation is useable:
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    void pop()
+    {
+        l.pop_back();
+    }
+    int top()
+    {
+        return l.back(); // access the last element
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+
 int main()
 {
     Node *head = NULL;
     Node *tail = NULL;
+
     return 0;
 }
