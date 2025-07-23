e05be62ace1e1a15eb21507ee7ea7f503d4bd0aa
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-23 15:49:19 +0600

    added

--- Module-14/Queue_implementation_with_Singly_Linked_LIst.cpp
+++ Module-14/Queue_implementation_with_Singly_Linked_LIst.cpp
@@ -70,10 +70,26 @@ public:
             return false;
         }
     }
-}
+};
 
 main()
 {
-
+    myQueue Q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        Q.push(val);
+    }
+    // print: 
+    while (!Q.empty())
+    {
+        cout << Q.front() << " ";
+        Q.pop();
+    }
+    
+     
     return 0;
 }
