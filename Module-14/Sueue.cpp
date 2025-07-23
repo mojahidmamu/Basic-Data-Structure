7f4816b211fe9c7f51931541d04c9c651607a807
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-23 15:08:52 +0600

    added

--- Module-14/Sueue.cpp
+++ Module-14/Sueue.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-mojahid
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
 int main()
 {
     
