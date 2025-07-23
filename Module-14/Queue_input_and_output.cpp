1e3a5e822d72474a89b09c9085836e78725bade2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-23 15:36:22 +0600

    added

--- Module-14/Queue_input_and_output.cpp
+++ Module-14/Queue_input_and_output.cpp
@@ -1,7 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+class Node
 {
-    
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val) // O(1)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void pop() // O(1)
+    {
+        sz--;
+        Node *deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+    }
+
+    int front() // O(1)
+    {
+        return head->val; // access the first element
+    }
+
+    int back() // O(1)
+    {
+        return tail->val; // access the last element
+    }
+
+    int size() // O(1)
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        if (head == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+int
+main()
+{
+
     return 0;
 }
