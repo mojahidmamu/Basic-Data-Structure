d270abb4c9184a48f4137893b0111fb78aaee749
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-23 15:25:00 +0600

    added

--- Module-14/Queue_implementation_with_Singly_Linked_LIst.cpp
+++ Module-14/Queue_implementation_with_Singly_Linked_LIst.cpp
@@ -14,13 +14,45 @@ public:
 
 class myQueeu
 {
-    public: 
-    Node *head = NULL
+public:
+    Node *head = NULL;
     Node *tail = NULL;
+
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+    
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back(); // access the last element
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
 }
 
-int main()
+int
+main()
 {
-    
+
     return 0;
 }
