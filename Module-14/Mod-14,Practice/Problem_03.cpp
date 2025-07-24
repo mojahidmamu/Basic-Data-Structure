b179fb91e27088058bfe788a93e3825229b2cbe8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-24 16:23:24 +0600

    added

--- Module-14/Mod-14,Practice/Problem_03.cpp
+++ Module-14/Mod-14,Practice/Problem_03.cpp
@@ -11,6 +11,25 @@ int main()
         cin >> val;
         st1.push(val);
     }
+    vector<int> tmp;
+    while (!st1.empty())
+    {
+        tmp.push_back(st1.top());
+        st1.pop();
+    }
+     
+
+    stack<int> st2;
+    for(int i = 0; i < tmp.size(); i++)
+    {
+         st2.push(tmp[i]);
+    }
+
+    while (!st2.empty())
+    {
+        cout << st2.top() << " ";
+        st2.pop();
+    }
     
 
     return 0;
