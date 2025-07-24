20878059dc952e811413c040d76714bd07d092b8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-24 16:07:06 +0600

    added

---
+++ Module-14/Mod-14,Practice/Problem_03.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> st1;
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int val;
+        cin >> val;
+        st1.push(val);
+    }
+
+    queue<int> q;
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    if (N != M)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    bool areEqual = true;
+    while (!st1.empty() && !q.empty())
+    {
+        if (st1.top() != q.front())
+        {
+            areEqual = false;
+            break;
+        }
+        st1.pop();
+        q.pop();
+    }
+
+    if (areEqual)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
