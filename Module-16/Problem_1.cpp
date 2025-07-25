a2a0c90dc539035dea1095e83855f472de5a267f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-25 10:57:30 +0600

    added

---
+++ Module-16/Problem_1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int n; 
+    
+    while (cin >> n && n != -1)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
