73fe65f1248896c0a351c9536982f3e4d8ae613e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-25 11:21:06 +0600

    added

--- Module-16/Special_queries.cpp
+++ Module-16/Special_queries.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int commandNumber;
+        cin >> commandNumber;
+        while (commandNumber == 0)
+        {
+            int personNmae;
+            cin >> personNmae;
+        }
+        
+    }
     return 0;
 }
