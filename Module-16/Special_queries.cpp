909d7d36e93d6f2c9a95357993aaa504daa8d8fb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-26 07:32:40 +0600

    added

--- Module-16/Special_queries.cpp
+++ Module-16/Special_queries.cpp
@@ -4,18 +4,27 @@ int main()
 {
     int TestCase;
     cin >> TestCase;
+    stack<int> st;
     while (TestCase--)
     {
         int commandNumber;
         cin >> commandNumber;
+        int personNmae;
         if (commandNumber == 0)
         {
-            int personNmae;
             cin >> personNmae;
         }
-        else
+        else if (commandNumber == 1)
         {
-            cout << "Invalid" << endl;
+            if (!st.empty())
+            {
+                cout << personNmae << endl;
+                st.pop();
+            }
+            else
+            {
+                cout << "Invalid" << endl;
+            }
         }
     }
     return 0;
