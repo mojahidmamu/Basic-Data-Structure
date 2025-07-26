b99149e28187a717e943315b87f9eba1ba83809c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-26 10:45:12 +0600

    added

--- Module-16/Elimination.cpp
+++ Module-16/Elimination.cpp
@@ -1,40 +1,76 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool isElimination(string s)
+// {
+//     stack<char> st;
+//     for (char ch : s)
+//     {
+//         if (ch == '1' && !st.empty() && st.top() == '0')
+//         {
+//             st.pop();
+//         }
+//         else 
+//         {
+//             st.push(ch);
+//         }
+        
+//     }
+//     return st.empty();
+// }
+
+// int main()
+// {
+//     int testcase;
+//     cin >> testcase;
+//     while (testcase--)
+//     {
+//         string s;
+//         cin >> s;
+//         if (isElimination(s))
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isElimination(string s)
-{
+string isEmptyAfterElimination(string &s) {
     stack<char> st;
-    for (char ch : s)
-    {
-        if (ch == '1' && !st.empty() && st.top() == '0')
-        {
+    for (char ch : s) {
+        if (!st.empty() && st.top() == '0' && ch == '1') {
             st.pop();
-        }
-        else 
-        {
+        } else {
             st.push(ch);
         }
-        
     }
-    return st.empty();
+    return st.empty() ? "YES" : "NO";
 }
 
 int main()
 {
-    int testcase;
-    cin >> testcase;
-    while (testcase--)
-    {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcase;
+    cin >> tcase;
+    while (tcase--) {
         string s;
         cin >> s;
-        if (isElimination(s))
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        cout << isEmptyAfterElimination(s) << '\n';
     }
 
     return 0;
