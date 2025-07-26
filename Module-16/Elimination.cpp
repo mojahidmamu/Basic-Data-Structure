1a58b85d383e7432d1ee4ab3c329c9c9edb48f15
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-26 10:10:19 +0600

    added

--- Module-16/Elimination.cpp
+++ Module-16/Elimination.cpp
@@ -1,13 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '0')
+        {
+            if (!st.empty() && st.top() == '1')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('0');
+            }
+        }
+        else if (c == '1')
+        {
+            if (!st.empty() && st.top() == '0')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('1');
+            }
+        }
+    }
+    return st.empty();
+}
+
 int main()
 {
-    int T;
-    cin >> T;
-    while (T--)
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
     {
-        
+        string s;
+        cin >> s;
+        if (isValid(s))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    
+
     return 0;
 }
