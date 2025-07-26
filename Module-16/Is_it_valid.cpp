fbfc20bde804f17c13a4a84d0f9f1d62195e2693
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-26 07:20:08 +0600

    added

--- Module-16/Is_it_valid.cpp
+++ Module-16/Is_it_valid.cpp
@@ -1,14 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '0')
+        {
+            if (!st.empty() && st.top() == '1')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('0');
+            }
+        }
+        else if (c == '1')
+        {
+            if (!st.empty() && st.top() == '0')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push('1');
+            }
+        }
+    }
+    return st.empty();
+}
+
 int main()
 {
-    int TestCase;
-    cin >> TestCase;
-    while (TestCase--)
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
     {
         string s;
         cin >> s;
+        if (isValid(s))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    
+
     return 0;
 }
