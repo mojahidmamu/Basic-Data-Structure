f7ad4bdc7bddaf4fbf70d8fa8cd81b01c9a9b6a4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-28 19:47:17 +0600

    added

--- Module-17/Binary_Tree.cpp
+++ Module-17/Binary_Tree.cpp
@@ -1,21 +1,24 @@
+/**
+ * 1. There is no way to back in Binary Tree.
+ * 2. Binary Tree has only two child, 
+ * There is no way to find any Name: Abdullah all Mojahid.
+ */
 #include <bits/stdc++.h>
 using namespace std;
 class Node
 {
 public:
     int val;
-    Node *next;
+    Node *left;
+    Node *right;
     Node(int val)
     {
         this->val = val;
-        this->next = NULL;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
-/**
- * 1. There is no way to back in Binary Tree.
- * 2. Binary Tree has only two child, 
- * There is no way to find any Name: Abdullah all Mojahid.
- */
+
 int main()
 {
     
