0344f5252b1eb744ef14df77f1a98be111fde5b3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-28 19:52:53 +0600

    added

--- Module-17/Binary_Tree.cpp
+++ Module-17/Binary_Tree.cpp
@@ -21,6 +21,11 @@ public:
 
 int main()
 {
-    
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
     return 0;
 }
