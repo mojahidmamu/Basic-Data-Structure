aa2c59db32d020bb1ed0c41ac41c3922534d381d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-07-31 09:54:52 +0600

    added

--- Module-19/STL_pair.cpp
+++ Module-19/STL_pair.cpp
@@ -16,9 +16,22 @@ public:
 
 int main()
 {
-    pair<int, int> p;
-    p = make_pair(10, 20);
-    cout << p.first << endl;
-    cout << p.second << endl;
+    // pair<int, int> p;
+    // p = make_pair(10, 20);
+    // cout << p.first << endl;
+    // cout << p.second << endl;
+
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+
     return 0;
 }
