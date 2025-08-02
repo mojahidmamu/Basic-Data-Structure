0a56ee3ad28fea385373759636633f3217b4aa4a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-02 16:50:37 +0600

    added

--- Module-20/c_plus.cpp
+++ Module-20/c_plus.cpp
@@ -1,7 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    
-    return 0;
-}
+// class class_name[নরমালি ক্যাপিটাল লেটারে লিখা হয়। না লিখলেও কাজ করবে]
+// {
+// public:
+//     [ access_modifier টোটাল ৩ ধরনের হয়ঃ Public, Private, Protected ] data_type variable_name;
+//     data_type variable_name;
+//     ...........
+// };
