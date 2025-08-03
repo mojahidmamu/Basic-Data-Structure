ba83f289d619535eac3de3e8ed8d111a6e9a639c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-03 10:14:30 +0600

    added

--- Module-21/Search_in_BST.cpp
+++ Module-21/Search_in_BST.cpp
@@ -14,8 +14,77 @@ public:
     }
 };
 
+Node *input_binary_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+      root = new Node(val);
+    }
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+
+
+    if (root == NULL)
+    {
+        cout << "No Tree" << endl;
+        return NULL;
+    }
+    while (!q.empty())
+    {
+        // Step- 1.
+        Node *p = q.front();
+        q.pop();
+        // Step- 2.
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+        {
+            myLeft = NULL;
+        }
+        else
+        {
+            myLeft = new Node(l);
+        }
+        //
+        if (r == -1)
+        {
+            myRight = NULL;
+        }
+        else
+        {
+            myRight = new Node(r);
+        }
+
+
+        p->left = myLeft;
+        p->right = myRight;
+        // Step- 3.
+        if (p->left != NULL)
+        {
+            q.push(p->left);
+        }
+        if (p->right != NULL)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+
 int main()
 {
-    
+    Node *root =  input_binary_tree();
     return 0;
 }
