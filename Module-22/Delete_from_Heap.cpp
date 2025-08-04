064b54df737d40aa8faf914a8fe10ed94ff9e51a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-04 10:33:33 +0600

    added

--- Module-22/Delete_from_Heap.cpp
+++ Module-22/Delete_from_Heap.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// insert at vector: 
+void insert_in_heap(vector<int> &v, val)
+{
+    
+}
 int main()
 {
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+
+    }
     
     return 0;
 }
