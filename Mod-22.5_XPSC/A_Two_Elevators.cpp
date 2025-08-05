995af748098bc010a3b52182c95b5c0849b44045
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-05 09:01:35 +0600

    added

--- Mod-22.5_XPSC/A_Two_Elevators.cpp
+++ Mod-22.5_XPSC/A_Two_Elevators.cpp
@@ -8,7 +8,20 @@ int main()
     {
         int a, b, c;
         cin >> a >> b >> c;
-        
+        int T1 = a - 1;
+        int T2 = (b - c) + (c - 1);
+        if (T1 > T2)
+        {
+            cout << 1 << endl;
+        }
+        else if (T1 < T2)
+        {
+            cout << 2 << endl;
+        }
+        else if (T1 == T2)
+        {
+            cout << 3 << endl;
+        }
     }
 
     return 0;
