9ba9ad495e131313ba83b2f6600705c71ad395c8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-05 09:09:05 +0600

    added

---
+++ Mod-22.5_XPSC/A_Phone_Desktop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
+
+    return 0;
+}
--- Mod-22.5_XPSC/A_Two_Elevators.cpp
+++ Mod-22.5_XPSC/A_Two_Elevators.cpp
@@ -9,16 +9,16 @@ int main()
         int a, b, c;
         cin >> a >> b >> c;
         int T1 = a - 1;
-        int T2 = (b - c) + (c - 1);
-        if (T1 > T2)
+        int T2 = abs(b - c) + (c - 1);
+        if (T1 < T2)
         {
             cout << 1 << endl;
         }
-        else if (T1 < T2)
+        else if (T1 > T2)
         {
             cout << 2 << endl;
         }
-        else if (T1 == T2)
+        else 
         {
             cout << 3 << endl;
         }
