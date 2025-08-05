ddead8bc72ee7fec9e65804bc297846c87d0f6d2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-05 09:23:43 +0600

    added

--- Mod-22.5_XPSC/A_Phone_Desktop.cpp
+++ Mod-22.5_XPSC/A_Phone_Desktop.cpp
@@ -8,6 +8,11 @@ int main()
     {
         int x, y;
         cin >> x >> y;
+        
+        int cellsPerScreen = 15;                                         
+        int usedCells = y * 4 + x;                                     
+        int screens = (usedCells + cellsPerScreen - 1) / cellsPerScreen; 
+        cout << screens << "\n";
     }
 
     return 0;
