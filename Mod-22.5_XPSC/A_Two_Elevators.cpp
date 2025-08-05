88b6c080e26bed03893f590a9d2d1512883b1dd7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-05 08:46:34 +0600

    added

--- Mod-22.5_XPSC/A_Two_Elevators.cpp
+++ Mod-22.5_XPSC/A_Two_Elevators.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+    }
+
     return 0;
 }
