d503364b9443ec6444c9043ac83032a41e3aa7e8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 12:11:20 +0600

    added

--- Module-23/Priority_Queue.cpp
+++ Module-23/Priority_Queue.cpp
@@ -3,13 +3,37 @@ using namespace std;
 int main()
 {
     priority_queue<int> pq;
-    pq.push(10);
-    pq.push(20);
-    pq.push(30);
-    pq.push(40);
-    cout << pq.top() << endl; // 40
-    pq.push(100);
-    cout << pq.top() << endl; // 1000
-    cout << pq.empty() << endl;
+    while (true)
+    {
+        int c;
+        cin >> c;
+        if (c == 0)
+        {
+            int x;
+            cin >> x;
+            pq.push(x);
+        }
+        else if (c == 1)
+        {
+            pq.pop();
+        }
+        else if (c == 2)
+        {
+            cout << pq.top() << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // pq.push(10);
+    // pq.push(20);
+    // pq.push(30);
+    // pq.push(40);
+    // cout << pq.top() << endl; // 40
+    // pq.push(100);
+    // cout << pq.top() << endl; // 1000
+    // cout << pq.empty() << endl;
     return 0;
 }
