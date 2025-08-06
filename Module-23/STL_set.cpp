68645d07f7ebcfb333d5bf87efa23f7783bdfb18
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 14:02:57 +0600

    added

--- Module-23/STL_set.cpp
+++ Module-23/STL_set.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    set<int> s; // set -> DSA
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int val;
+        cin >> val;
+        s.insert(val); // insert value in set
+    }
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
     return 0;
 }
