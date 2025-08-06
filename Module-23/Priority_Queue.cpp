a7e35c62bcffafd1dd99ce55d9dcc3110b3000e8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 09:37:37 +0600

    added

--- Module-23/Priority_Queue.cpp
+++ Module-23/Priority_Queue.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
-    
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(20);
+    pq.push(30);
+    pq.push(40);
+    cout << pq.top() << endl;
     return 0;
 }
