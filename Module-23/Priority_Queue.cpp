3ddd22332b51bf70dfef75ce7767c0cca24bfefe
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 11:04:06 +0600

    added

--- Module-23/Priority_Queue.cpp
+++ Module-23/Priority_Queue.cpp
@@ -7,6 +7,9 @@ int main()
     pq.push(20);
     pq.push(30);
     pq.push(40);
-    cout << pq.top() << endl;
+    cout << pq.top() << endl; // 40
+    pq.push(100);
+    cout << pq.top() << endl; // 1000
+    cout << pq.empty() << endl;
     return 0;
 }
