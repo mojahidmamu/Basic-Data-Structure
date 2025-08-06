2d83b8f86ba12210ebe4f54c4065b28ef60d28db
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 12:18:09 +0600

    added

--- Module-23/STL_map.cpp
+++ Module-23/STL_map.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-    
+    map<string, int> mp;
+    mp["Sakib al hasn"] = 79;
+    mp.insert({"akib", 100});
+    mp.insert({"akib", 150});
+    mp["tamim"] = 79;
+
+    for(auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    cout << mp.count("akib") << endl;
     return 0;
 }
