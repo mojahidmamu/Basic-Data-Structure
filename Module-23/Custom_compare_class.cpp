a64ba90a50cfda409ad9fe1228171b11cacb437d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-06 11:12:08 +0600

    added

--- Module-23/Custom_compare_class.cpp
+++ Module-23/Custom_compare_class.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+    Student(string name, int roll, int marks)
+    {
+        this->name = name;
+        this->roll = roll;
+        this->marks = marks;
+    }
+};
 int main()
 {
     int T;
