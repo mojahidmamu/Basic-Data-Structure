725f6a1fac56e0f906e7a1ca1d7ccbd21441bb32
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-08 07:04:34 +0600

    added

--- Module-23/A_Phone_Desktop.cpp
+++ Module-23/A_Phone_Desktop.cpp
@@ -6,8 +6,26 @@ int main()
     cin >> TestCase;
     while (TestCase--)
     {
-        
+        int x, y;
+        cin >> x >> y;
+        int Screen = y / 2;
+        int blank = Screen * 7;
+        if (y % 2 == 1)
+        {
+            Screen++;
+            blank += 11;
+        }
+        if (x > blank)
+        {
+            x -= blank;
+            Screen += (x / 15);
+            if (x % 15 != 0)
+            {
+                Screen++;
+            }
+        }
+        cout << Screen << endl;
     }
-    
+
     return 0;
 }
