829f3fab0d25336a12356719328a27952f9ca38d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-08 07:18:39 +0600

    added

--- Module-24/Sorted.cpp
+++ Module-24/Sorted.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 int main()
 {
+    set<int> s; // set -> DSA
     int TestCase;
     cin >> TestCase;
     while (TestCase--)
     {
-        
+        int N;
+        cin >> N;
+        int arr[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + N);
+        for (int i = 0; i < N; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-    
+
     return 0;
 }
