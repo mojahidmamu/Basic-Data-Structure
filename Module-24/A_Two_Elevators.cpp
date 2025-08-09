d092c98348dafe130729aa248c094c80ec14a715
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-09 11:18:25 +0600

    added

--- Module-24/A_Two_Elevators.cpp
+++ Module-24/A_Two_Elevators.cpp
@@ -9,16 +9,20 @@ int main()
         int a, b, c;
         cin >> a >> b >> c;
 
-        int t1 = ;
-        int t2 = ;
+        int t1 = a - 1;
+        int t2 = abs(b - c) + (c - 1);
 
         if (t1 < t2)
         {
-            cout << t1 << endl;
+            cout << "1" << endl;
         }
-        else
+        else if ( t2 < t1)
         {
-            cout << t2 << endl;
+            cout << "2" << endl;
+        }
+        else if(t1 == t2)
+        {
+            cout << "3" << endl;
         }
     }
 
