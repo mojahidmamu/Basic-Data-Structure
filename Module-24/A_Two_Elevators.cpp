534132318df99550c60d83e6ffb523c3914deb44
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-09 11:13:01 +0600

    added

--- Module-24/A_Two_Elevators.cpp
+++ Module-24/A_Two_Elevators.cpp
@@ -6,8 +6,21 @@ int main()
     cin >> testCase;
     while (testCase--)
     {
-        
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int t1 = ;
+        int t2 = ;
+
+        if (t1 < t2)
+        {
+            cout << t1 << endl;
+        }
+        else
+        {
+            cout << t2 << endl;
+        }
     }
-    
+
     return 0;
 }
