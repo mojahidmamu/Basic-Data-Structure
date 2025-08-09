fca3cf9e20d6be0e14da89aa2cbce2a55026a0ec
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-09 06:21:48 +0600

    added

--- Module-24/Count_Me.cpp
+++ Module-24/Count_Me.cpp
@@ -4,17 +4,29 @@ int main()
 {
     int TestCase;
     cin >> TestCase;
+    cin.ignore();
     while (TestCase--)
     {
         string sentence;
         getline(cin, sentence);
         stringstream ss(sentence);
         string word;
+        map<string, int> count;
+
+        string maxWord;
+        int maxCount = 0;
         while (ss >> word)
         {
-            cout << word << " ";
+            count[word]++;
+            if (count[word] > maxCount)
+            {
+                maxCount = count[word];
+                maxWord = word;
+            }
         }
-        cout << endl;
+
+        cout << maxWord << " " << maxCount << endl;
     }
+
     return 0;
 }
