dc4893ca243b951d4296a1a04098a9a2cf2d889b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-10 18:35:59 +0600

    added

--- Module-24/hello.c
+++ Module-24/hello.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+// #include<stdio.h>
+// int main()
+// {
+//        // ৫ বার ভিতরের লুপটি চালানোর জন্য লুপ
+//    
+//         for(int j=1;j<=10;j++)   // ১ থেকে ১০ প্রিন্ট করার লুপ
+//         {
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+#include <stdio.h>
 int main()
 {
-    
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            printf("%d", j);
+        }
+    }
+    printf("/n");
     return 0;
 }
