33d6e335670aa6b75f906619619965c1773b2690
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-16 09:13:20 +0600

    added

--- Module-4/Duplicate.cpp
+++ Module-4/Duplicate.cpp
@@ -14,10 +14,10 @@ int main()
 
     int flag = 0;
 
-    for (int i = 1; i < N ; i++)
+    for (int i = 0; i < N-1 ; i++)
     {
          
-            if (arr[i] == arr[i-1])
+            if (arr[i] == arr[i+1])
             {
                 flag = 1;
             }
