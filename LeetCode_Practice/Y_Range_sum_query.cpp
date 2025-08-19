3ac895f20bb68f2d6cda08e4e7a84f71fb6bf34e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-19 20:33:41 +0600

    added

--- LeetCode_Practice/Y_Range_sum_query.cpp
+++ LeetCode_Practice/Y_Range_sum_query.cpp
@@ -1,21 +1,60 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int N, Q;
+//     cin >> N, Q;
+//     int A[N];
+//     for (int i = 0; i < N; i++) // O(N)
+//     {
+//         cin >> A[i];
+//     }
+//     for (int i = 1; i <= Q; i++) // O(Q)
+//     {
+//         int L, R;
+//         cin >> L, R;
+
+//         L--;
+//         R--;
+
+//         int sum = 0;
+//         for (int j = L; j <= R; j++)
+//         {
+//             sum += A[j];
+//         }
+//         cout << sum << endl;
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int N, Q;
-    cin >> N, Q;
-    int A[N];
-    for (int i = 0; i < N; i++)
+    cin >> N >> Q;
+    vector<int> V(N);
+    for (int i = 0; i < N; i++) // O(N)
     {
-        cin >> A[i];
+        cin >> V[i];
     }
-    for (int i = 0; i < Q; i++)
+
+    for (int i = 1; i <= Q; i++) // O(Q)
     {
         int L, R;
-        cin >> L, R;
+        cin >> L >> R;
+
+        L--;
+        R--; // converting the position to array index
+
+        int SUM = 0;
+        for (int j = L; j <= R; j++) // O(N)  
+        {
+            SUM += V[j];
+        }
+
+        cout << SUM << endl;
     }
-    
-    
-    
     return 0;
 }
