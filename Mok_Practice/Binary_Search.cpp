c2cd469c97c87b60a000f939e6e0cf4e24c1ecc9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-23 20:16:08 +0600

    added

--- Mok_Practice/Binary_Search.cpp
+++ Mok_Practice/Binary_Search.cpp
@@ -9,7 +9,30 @@ int main()
     {
         cin >> arr[i];
     }
-    sort(arr, arr + N);j
-    
+    sort(arr, arr + N);
+    int target;
+    cin >> target;
+
+    int low = 0, high = N - 1;
+    while (low <= high)
+    {
+        int mid_index = (low + high) / 2;
+
+        if (arr[mid_index] == target)
+        {
+            cout << "Found at index: " << mid_index << endl;
+            return 0;
+        }
+        else if (arr[mid_index] < target)
+        {
+            low = mid_index + 1;
+        }
+        else
+        {
+            high = mid_index - 1;
+        }
+    }
+
+    cout << "No found" << endl;
     return 0;
 }
