7c9523d3f97339811bdb99fd8d0350e8e78eada1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-23 20:07:32 +0600

    added

--- Mok_Practice/Binary_Search.cpp
+++ Mok_Practice/Binary_Search.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);j
     
     return 0;
 }
