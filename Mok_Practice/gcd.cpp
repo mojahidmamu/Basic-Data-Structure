b8e8faf3cbd6421854aa474031603728127dd917
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-23 20:20:38 +0600

    added

--- Mok_Practice/gcd.cpp
+++ Mok_Practice/gcd.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
 int main()
 {
     int a, b;
     cin >> a >> b;
-    cout << gcd(a, b) << enld;
+    cout << gcd(a, b) << endl;
     return 0;
 }
